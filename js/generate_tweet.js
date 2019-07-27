ef697117610dfa58989ac053a5a54be7600624fa
pythondude325 <gischeaffer@gmail.com>
2019-07-27 16:11:52 -0500

    Finished tweet generator and basic glue code

---
+++ js/generate_tweet.js
@@ -0,0 +1,73 @@
+
+
+function generateTweet(model){
+
+    function generateTokens(model){
+        let end_index = model.items.indexOf("End");
+
+        let output = [0];
+
+        let current_symbols = [0];
+
+        while(current_symbols[current_symbols.length - 1] != end_index){
+            let next_set = model.symbols[current_symbols.map(n => n.toString()).join(" ")];
+
+            let next_symbol = weightedChoose(next_set);
+
+            output.push(next_symbol);
+
+            if(current_symbols.length == model.order){
+                current_symbols.shift();
+            }
+
+            current_symbols.push(next_symbol);
+        }
+
+        return output;
+    }
+
+    function generateTextParts(model, tokens){
+        let output = [];
+
+        for(let token of tokens){
+            let part = model.items[token];
+
+            if(part.hasOwnProperty("Mid")){
+                output.push(part["Mid"]);
+            }
+        }
+
+        return output;
+    }
+
+    function isWord(o){
+        return o.hasOwnProperty("Word");
+    }
+
+    function isPunctuation(o){
+        return o.hasOwnProperty("Punctuation");
+    }
+
+    function generateText(textParts){
+        let output = "";
+
+        for(let i = 0; i < textParts.length; i++){
+            let current_part = textParts[i];
+            let next_part = textParts[i + 1] || {"End": ""};
+
+            if(isWord(current_part)){
+                let space_after = isWord(next_part) || (isPunctuation(next_part) && next_part["Punctuation"][1]);
+
+                output += current_part["Word"] + (space_after?" ":"");
+            } else if(isPunctuation(current_part)){
+                let space_after = current_part["Punctuation"][2] && !(next_part.hasOwnProperty("End"));
+
+                output += current_part["Punctuation"][0] + (space_after?" ":"");
+            }
+        }
+
+        return output;
+    }
+
+    return generateText(generateTextParts(model, generateTokens(model)));
+}
---
+++ js/main.js
@@ -0,0 +1,24 @@
+// From https://stackoverflow.com/a/34064434
+function htmlDecode(input) {
+  var doc = new DOMParser().parseFromString(input, "text/html");
+  return doc.documentElement.textContent;
+}
+
+async function main(){
+    let response = await fetch("data/trump_model.json");
+    const trump_model = await response.json();
+
+
+    const container = document.getElementById("trump-tweet");
+    const button = document.getElementById("new-tweet-button");
+
+    function generate(){
+        container.innerText = htmlDecode(generateTweet(trump_model));
+    }
+
+    generate();
+
+    button.addEventListener("click", generate);
+}
+
+document.addEventListener("DOMContentLoaded", main);
